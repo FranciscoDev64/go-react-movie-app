af4b130ad6424d61ed45e70e7adb427ba84c9849
franciscogonz64 <francisco.gonz64@gmail.com>
2026-01-15 18:21:20 -0800

    Add genre selection, add/edit/select component foundation

--- go-movies-back-end/cmd/api/routes.go
+++ go-movies-back-end/cmd/api/routes.go
@@ -21,10 +21,14 @@ func (app *application) routes() http.Handler {
 	mux.Get("/logout", app.logout)
 
 	mux.Get("/movies", app.AllMoives)
+	mux.Get("/movies/{id}", app.GetMovie)
+
+	mux.Get("/genres", app.AllGenres)
 
 	mux.Route("/admin", func(mux chi.Router) {
 		mux.Use(app.authRequired)
 		mux.Get("/movies", app.MovieCatalog)
+		mux.Get("/movies/{id}", app.MovieForEdit)
 	})
 
 	return mux
--- go-movies-back-end/internal/repository/repository.go
+++ go-movies-back-end/internal/repository/repository.go
@@ -10,4 +10,8 @@ type DatabaseRepo interface {
 	Connection() *sql.DB
 	GetUserByEmail(email string) (*models.User, error)
 	GetUserByID(id int) (*models.User, error)
+
+	OneMovie(id int) (*models.Movie, error)
+	OneMovieForEdit(id int) (*models.Movie, []*models.Genre, error)
+	AllGenres() ([]*models.Genre, error)
 }
